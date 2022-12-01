986560125a30660f1389f8f302dabf5f2a536d20
Albin Bramstång <albin.bramstang@devies.se>
2022-12-01 16:32:23 +0100

    Cleanup

--- Solutions/Program.cs
+++ Solutions/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("*** Advent of Code 2022 ***");
 // var response = await client.GetAsync("https://adventofcode.com/2022/day/1/input"); 
 // var data = await response.Content.ReadAsStringAsync();
 
-var data = await File.ReadAllLinesAsync("Day1/Day1.txt");
+var data = await File.ReadAllLinesAsync("Day1/Data.txt");
 
 var part1 = Solution.SolvePart1(data);
 var part2 = Solution.SolvePart2(data);
--- Tests/Day1/Tests.cs
+++ Tests/Day1/Tests.cs
@@ -1,13 +1,13 @@
 using Solutions.Day1;
 
-namespace Tests;
+namespace Tests.Day1;
 
-public class Day1Tests
+public class Tests
 {
     [Fact]
     public async Task Part1()
     {
-        var data = await File.ReadAllLinesAsync("Data/Day1.txt");
+        var data = await File.ReadAllLinesAsync("Day1/Data.txt");
         var result = Solution.SolvePart1(data);
         Assert.Equal("24000", result);
     }
@@ -15,7 +15,7 @@ public class Day1Tests
     [Fact]
     public async Task Part2()
     {
-        var data = await File.ReadAllLinesAsync("Data/Day1.txt");
+        var data = await File.ReadAllLinesAsync("Day1/Data.txt");
         var result = Solution.SolvePart2(data);
         Assert.Equal("45000", result);
     }
