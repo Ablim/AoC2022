7d2600b3d6603c9743b80dd4a3578d3925cd65fa
Albin Bramstång <albin.bramstang@devies.se>
2022-12-12 23:56:50 +0100

    i give up

--- Solutions/Day9/Knot.cs
+++ Solutions/Day9/Knot.cs
@@ -2,31 +2,7 @@ namespace Solutions.Day9;
 
 public record Knot
 {
-    private int _row;
-    private int _column;
-    
-    public int Row
-    {
-        get => _row;
-        set
-        {
-            PreviousRow = _row;
-            PreviousColumn = _column;
-            _row = value;
-        }
-    }
-
-    public int Column
-    {
-        get => _column;
-        set
-        {
-            PreviousRow = _row;
-            PreviousColumn = _column;
-            _column = value;
-        }
-    }
+    public int Row { get; set; }
+    public int Column { get; set; }
     public string Name { get; set; }
-    public int PreviousRow { get; private set; }
-    public int PreviousColumn { get; private set; }
 }
