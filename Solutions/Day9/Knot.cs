aab81499b3a05c9c5766af856da3315c19145504
Albin Bramstång <albin.bramstang@devies.se>
2022-12-14 07:26:50 +0100

    Fix nullable prop

--- Solutions/Day9/Knot.cs
+++ Solutions/Day9/Knot.cs
@@ -4,5 +4,5 @@ public record Knot
 {
     public int Row { get; set; }
     public int Column { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
