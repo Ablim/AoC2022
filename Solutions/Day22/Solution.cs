2efedc9ac0f76a95eab3e894c09265cfed4dc84c
Albin Bramstång <albin.bramstang@devies.se>
2022-12-22 16:42:58 +0100

    Day 22 prep

---
+++ Solutions/Day22/Solution.cs
@@ -0,0 +1,16 @@
+namespace Solutions.Day22;
+
+public static class Solution
+{
+    public static int Day => 22;
+    
+    public static string SolvePart1(IEnumerable<string> data)
+    {
+        return "";
+    }
+
+    public static string SolvePart2(IEnumerable<string> data)
+    {
+        return "";
+    }
+}
---
+++ Tests/Day22/Tests.cs
@@ -0,0 +1,22 @@
+using Solutions.Day22;
+
+namespace Tests.Day22;
+
+public class Tests
+{
+    [Fact]
+    public async Task Part1()
+    {
+        var data = await File.ReadAllLinesAsync($"Day{Solution.Day}/Data.txt");
+        var result = Solution.SolvePart1(data);
+        Assert.Equal("6032", result);
+    }
+    
+    [Fact]
+    public async Task Part2()
+    {
+        var data = await File.ReadAllLinesAsync($"Day{Solution.Day}/Data.txt");
+        var result = Solution.SolvePart2(data);
+        Assert.Equal("301", result);
+    }
+}
